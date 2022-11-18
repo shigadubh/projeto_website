44a4ff20003724cc0e53eba33ed3cbdda04f6606
Lucca Broering <113954095+shigadubh@users.noreply.github.com>
2022-11-18 11:48:47 -0300

    Add files via upload

---
+++ COFFEE/javaScript/script.js
@@ -0,0 +1,12 @@
+function locate(){
+	alert("You are here!");
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
